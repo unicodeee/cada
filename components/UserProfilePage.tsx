0d0bbfcf979003974256d0880d3571683b01b4db
Thang Nguyen <unicodeee2000@gmail.com>
2025-03-29 09:36:56 -0700

    fix image source when using Image in nextjs

--- components/UserProfilePage.tsx
+++ components/UserProfilePage.tsx
@@ -3,6 +3,7 @@
 import SignOutButton from "@components/SignOutButton";
 
 import Image from "next/image"
+import React from "react";
 
 interface UserProfileProps {
     name: string;
@@ -14,11 +15,8 @@ const UserProfile = ({ name, email, image }: UserProfileProps) => {
     return (
         <div className="flex flex-col items-center justify-center w-full max-w-md p-6 bg-white shadow-lg rounded-lg dark:bg-gray-800 dark:text-white">
             <div className="flex flex-col items-center gap-4">
-                <Image
-                    src={image || "/emptyAvatar.jpg"} // fallback to a default image if no image is provided
-                    alt="Profile Picture"
-                    className="w-32 h-32 rounded-full object-cover"
-                />
+                <Image src={image || "/emptyAvatar.jpg"} alt="Profile Picture" className="w-32 h-32 rounded-full object-cover" width={500} height={500}/>
+
                 <h2 className="text-2xl font-bold">{name}</h2>
                 <p className="text-sm text-gray-500">{email}</p>
             </div>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+                search: '',
+            },
+        ],
+    },
+}
