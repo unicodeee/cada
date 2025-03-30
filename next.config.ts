66317a7d494633eb8a2f9ca6e0668da679b6bb44
Thang Nguyen <unicodeee2000@gmail.com>
2025-03-29 23:34:33 -0700

    same previous

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ module.exports = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
+                hostname: 'lh3.googleusercontent.com', // because using Image, next needs to know the images come from a given domain
                 port: '',
                 pathname: '/**',
                 search: '',
